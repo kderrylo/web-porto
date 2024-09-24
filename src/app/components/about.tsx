9d4a97fe10c03a7122508f9c253e2dc93e09f7fa
derryl <derrylzodxyt_gmail.com>
2024-09-24 09:08:37 +0700

    Image fixing for deployment

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Image from 'next/image';
 
 import background from "../../../public/assets/bg-2.webp";
 import Link from "next/link";
@@ -72,55 +73,55 @@ export default function About() {
                   <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 ">
                     {/* Skill 1 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-orange-700">
-                      <img src="/assets/logo-html.png" alt="Logo 1" className="w-6 h-6" />
+                      <Image src="/assets/logo-html.png" alt="Logo 1" width={24} height={24} />
                       <span className="text-sm font-bold ">HTML</span>
                     </div>
 
                     {/* Skill 2 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-blue-900">
-                      <img src="/assets/logo-css.png" alt="Logo 2" className="w-6 h-6" />
+                      <Image src="/assets/logo-css.png" alt="Logo 2" width={24} height={24} />
                       <span className="text-sm font-bold">CSS</span>
                     </div>
 
                     {/* Skill 3 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-yellow-500">
-                      <img src="/assets/logo-js.png" alt="Logo 3" className="w-6 h-6" />
+                      <Image src="/assets/logo-js.png" alt="Logo 3" width={24} height={24} />
                       <span className="text-sm text-black font-bold">JavaScript</span>
                     </div>
 
                     {/* Skill 4 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-cyan-700">
-                      <img src="/assets/logo-ts.png" alt="Logo 4" className="w-6 h-6" />
+                      <Image src="/assets/logo-ts.png" alt="Logo 4" width={24} height={24} />
                       <span className="text-sm font-bold">TypeScript</span>
                     </div>
 
                     {/* Skill 5 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-orange-600">
-                      <img src="/assets/logo-git.png" alt="Logo 5" className="w-6 h-6" />
+                      <Image src="/assets/logo-git.png" alt="Logo 5" width={24} height={24} />
                       <span className="text-sm font-bold">Git</span>
                     </div>
 
                     {/* Skill 6 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-purple-600">
-                      <img src="/assets/logo-figma.png" alt="Logo 6" className="w-6 h-6" />
+                      <Image src="/assets/logo-figma.png" alt="Logo 6" width={24} height={24} />
                       <span className="text-sm font-bold">Figma</span>
                     </div>
 
                     {/* Skill 7 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-cyan-500">
-                      <img src="/assets/logo-react.png" alt="Logo 7" className="w-6 h-6" />
+                      <Image src="/assets/logo-react.png" alt="Logo 7" width={24} height={24} />
                       <span className="text-sm text-black font-bold">React.Js</span>
                     </div>
 
                     {/* Skill 8 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-light">
-                      <img src="/assets/logo-nextjs.png" alt="Logo 8" className="w-6 h-6" />
+                      <Image src="/assets/logo-nextjs.png" alt="Logo 8" width={24} height={24} />
                       <span className="text-sm text-black font-bold">Next.Js</span>
                     </div>
 
                     {/* Skill 9 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-cyan-700">
-                      <img src="/assets/logo-tw.png" alt="Logo 9" className="w-6 h-6" />
+                      <Image src="/assets/logo-tw.png" alt="Logo 9" width={24} height={24} />
                       <span className="text-sm font-bold">Tailwindcss</span>
                     </div>
 
@@ -128,19 +129,19 @@ export default function About() {
 
                     {/* Skill 10 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-light">
-                      <img src="/assets/logo-flask.png" alt="Logo 9" className="w-6 h-6" />
+                      <Image src="/assets/logo-flask.png" alt="Logo 9" width={24} height={24} />
                       <span className="text-sm text-black font-bold">Flask</span>
                     </div>
 
                     {/* Skill 11 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-cyan-200">
-                      <img src="/assets/logo-java.png" alt="Logo 19" className="w-6 h-6" />
+                      <Image src="/assets/logo-java.png" alt="Logo 19" width={24} height={24} />
                       <span className="text-sm text-black font-bold">Java</span>
                     </div>
 
                     {/* Skill 11 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-light">
-                      <img src="/assets/logo-sqlite.png" alt="Logo 19" className="w-6 h-6" />
+                      <Image src="/assets/logo-sqlite.png" alt="Logo 19" width={24} height={24} />
                       <span className="text-sm text-black font-bold">SQLite</span>
                     </div>
 
@@ -154,55 +155,55 @@ export default function About() {
                   <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 ">
                     {/* Skill 1 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-blue-900">
-                      <img src="/assets/logo-python2.png" alt="Logo 1" className="w-6 h-6" />
+                      <Image src="/assets/logo-python2.png" alt="Logo 1" width={24} height={24} />
                       <span className="text-sm font-bold ">Python</span>
                     </div>
 
                     {/* Skill 2 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-light">
-                      <img src="/assets/logo-pandas.png" alt="Logo 2" className="w-6 h-6" />
+                      <Image src="/assets/logo-pandas.png" alt="Logo 2" width={24} height={24} />
                       <span className="text-sm text-black font-bold">Pandas</span>
                     </div>
 
                     {/* Skill 3 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-cyan-400">
-                      <img src="/assets/logo-numpy.png" alt="Logo 3" className="w-6 h-6" />
+                      <Image src="/assets/logo-numpy.png" alt="Logo 3" width={24} height={24} />
                       <span className="text-sm text-black font-bold">NumPy</span>
                     </div>
 
                     {/* Skill 4 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-light">
-                      <img src="/assets/logo-matplotlib.png" alt="Logo 4" className="w-6 h-6" />
+                      <Image src="/assets/logo-matplotlib.png" alt="Logo 4" width={24} height={24} />
                       <span className="text-sm text-black font-bold">Matplotlib</span>
                     </div>
 
                     {/* Skill 5 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-blue-800">
-                      <img src="/assets/logo-seaborn.png" alt="Logo 5" className="w-6 h-6" />
+                      <Image src="/assets/logo-seaborn.png" alt="Logo 5" width={24} height={24} />
                       <span className="text-sm font-bold">Seaborn</span>
                     </div>
 
                     {/* Skill 6 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-light">
-                      <img src="/assets/logo-scikit-learn.png" alt="Logo 6" className="w-6 h-6" />
+                      <Image src="/assets/logo-scikit-learn.png" alt="Logo 6" width={24} height={24} />
                       <span className="text-sm text-black font-bold">Scikit-learn</span>
                     </div>
 
                     {/* Skill 7 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-orange-600">
-                      <img src="/assets/logo-tf.png" alt="Logo 7" className="w-6 h-6" />
+                      <Image src="/assets/logo-tf.png" alt="Logo 7" width={24} height={24} />
                       <span className="text-sm text-black font-bold">TensorFlow</span>
                     </div>
 
                     {/* Skill 8 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-light">
-                      <img src="/assets/logo-keras.png" alt="Logo 8" className="w-6 h-6" />
+                      <Image src="/assets/logo-keras.png" alt="Logo 8" width={24} height={24} />
                       <span className="text-sm text-black font-bold">Keras</span>
                     </div>
 
                     {/* Skill 9 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-yellow-600">
-                      <img src="/assets/logo-opencv.png" alt="Logo 9" className="w-6 h-6" />
+                      <Image src="/assets/logo-opencv.png" alt="Logo 9" width={24} height={24} />
                       <span className="text-sm font-bold">OpenCV</span>
                     </div>
 
@@ -210,21 +211,16 @@ export default function About() {
 
                     {/* Skill 10 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-light">
-                      <img src="/assets/logo-nltk.png" alt="Logo 9" className="w-5 h-5" />
+                      <Image src="/assets/logo-nltk.png" alt="Logo 9" width={24} height={24} />
                       <span className="text-sm text-black font-bold">NLTK</span>
                     </div>
 
                     {/* Skill 11 */}
                     <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-cyan-300">
-                      <img src="/assets/logo-librosa.png" alt="Logo 19" className="w-6 h-6" />
+                      <Image src="/assets/logo-librosa.png" alt="Logo 19" width={24} height={24} />
                       <span className="text-sm text-black font-bold">Librosa</span>
                     </div>
 
-                    {/* Skill 11 */}
-                    {/* <div className="flex items-center justify-center space-x-4 p-2 rounded-lg shadow-lg bg-light">
-                      <img src="/assets/logo-sqlite.png" alt="Logo 19" className="w-6 h-6" />
-                      <span className="text-sm text-black font-bold">IPython</span>
-                    </div> */}
 
 
 
